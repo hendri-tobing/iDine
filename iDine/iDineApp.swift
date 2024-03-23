7fc5168e085ca9aacfd8b9d67982fd0b184972af
Hendri Tobing <tempa.staff@gmail.com>
2024-03-23 13:51:49 +0700

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Triana Ambarsari on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
